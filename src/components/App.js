ce2937fb0aef07f74b963ec586bd02b9cf14c2e9
ramankarki <ramankarki40@gmail.com>
2021-01-04 22:17:18 +0545

    imported streams components and added to Route component under BrowserRouter

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,24 @@
 import React from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import StreamCreate from "./streams/StreamCreate";
+import StreamShow from "./streams/StreamShow";
+import StreamEdit from "./streams/StreamEdit";
+import StreamDelete from "./streams/StreamDelete";
+import StreamList from "./streams/StreamList";
 
 export default function App() {
-  return <div>hello world</div>;
+  return (
+    <section>
+      <header></header>
+      <BrowserRouter>
+        <section>
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/create" exact component={StreamCreate} />
+          <Route path="/streams/show" exact component={StreamShow} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+        </section>
+      </BrowserRouter>
+    </section>
+  );
 }
