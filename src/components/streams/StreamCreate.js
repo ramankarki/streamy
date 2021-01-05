85b05e4e36db0c12592508ae838552acfa890e27
ramankarki <ramankarki40@gmail.com>
2021-01-05 20:38:47 +0545

    imported createStream action creater and connect function to wire action creater and form validation, stream get created onSubmit

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
+import { connect } from "react-redux";
+import { createStream } from "../../actions";
 
 class StreamCreate extends React.Component {
   renderError({ error, touched }) {
@@ -13,7 +15,6 @@ class StreamCreate extends React.Component {
   }
 
   renderInput = (formProps) => {
-    console.log(formProps);
     return (
       <div className="field">
         <label>{formProps.label}</label>
@@ -23,9 +24,9 @@ class StreamCreate extends React.Component {
     );
   };
 
-  onSubmit(formValues) {
-    console.log(formValues);
-  }
+  onSubmit = (formValues) => {
+    this.props.createStream(formValues);
+  };
 
   render() {
     return (
@@ -59,7 +60,9 @@ const validate = (formValues) => {
   return errors;
 };
 
-export default reduxForm({
+const formWrapped = reduxForm({
   form: "streamCreate",
   validate,
 })(StreamCreate);
+
+export default connect(null, { createStream })(formWrapped);
