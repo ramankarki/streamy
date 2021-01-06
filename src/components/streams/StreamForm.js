bf59f54b3c6769d82db12f942c4adec36df4c920
ramankarki <ramankarki40@gmail.com>
2021-01-06 15:12:21 +0545

    created a separate StreamForm component that can be reused by StreamCreate and StreamEdit component

---
+++ src/components/streams/StreamForm.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { Field, reduxForm } from "redux-form";
+
+class StreamForm extends React.Component {
+  renderError({ error, touched }) {
+    if (error && touched) {
+      return (
+        <div style={{ color: "#fc5185" }}>
+          <div>{error}</div>
+        </div>
+      );
+    }
+  }
+
+  renderInput = (formProps) => {
+    return (
+      <div className="field">
+        <label>{formProps.label}</label>
+        <input {...formProps.input} autoComplete="off" />
+        {this.renderError(formProps.meta)}
+      </div>
+    );
+  };
+
+  onSubmit = (formValues) => {
+    this.props.onSubmit(formValues);
+  };
+
+  render() {
+    return (
+      <form
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        className="ui form"
+      >
+        <Field name="title" component={this.renderInput} label="Title" />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Description"
+        />
+        <button className="ui button primary">Create Stream</button>
+      </form>
+    );
+  }
+}
+
+const validate = (formValues) => {
+  const errors = {};
+
+  if (!formValues.title) {
+    errors.title = "You must enter a title.";
+  }
+
+  if (!formValues.description) {
+    errors.description = "You must enter a description";
+  }
+
+  return errors;
+};
+
+export default reduxForm({
+  form: "streamForm",
+  validate,
+})(StreamForm);
