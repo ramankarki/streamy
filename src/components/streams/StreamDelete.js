4cd2c3222f0dfe7dd0dd4a8cd82bd05639c0a0f9
ramankarki <ramankarki40@gmail.com>
2021-01-06 21:00:20 +0545

    refractored to class based component and passed actions and onDmiss functions as props

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,24 +1,30 @@
 import React from "react";
 import Modal from "../../components/Modal";
+import history from "../../history";
 
-const StreamDelete = () => {
-  const actions = (
-    <React.Fragment>
-      <button className="ui button negative">Delete</button>
-      <button className="ui button">Cancel</button>
-    </React.Fragment>
-  );
+class StreamDelete extends React.Component {
+  actions = () => {
+    return (
+      <React.Fragment>
+        <button className="ui button negative">Delete</button>
+        <button className="ui button">Cancel</button>
+      </React.Fragment>
+    );
+  };
 
-  return (
-    <div>
-      <h3>Stream Delete</h3>
-      <Modal
-        title="Delete Stream"
-        content="Are you sure you want to delete this stream ?"
-        actions={actions}
-      />
-    </div>
-  );
-};
+  render() {
+    return (
+      <div>
+        <h3>Stream Delete</h3>
+        <Modal
+          title="Delete Stream"
+          content="Are you sure you want to delete this stream ?"
+          actions={this.actions()}
+          onDismiss={() => history.push("/")}
+        />
+      </div>
+    );
+  }
+}
 
 export default StreamDelete;
