11ca3c67b46d372f8e9fce6487412914eee2a0c4
ramankarki <ramankarki40@gmail.com>
2021-01-06 20:47:19 +0545

    imported modal and used for delete stream

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,7 +1,24 @@
 import React from "react";
+import Modal from "../../components/Modal";
 
 const StreamDelete = () => {
-  return <div>stream delete</div>;
+  const actions = (
+    <React.Fragment>
+      <button className="ui button negative">Delete</button>
+      <button className="ui button">Cancel</button>
+    </React.Fragment>
+  );
+
+  return (
+    <div>
+      <h3>Stream Delete</h3>
+      <Modal
+        title="Delete Stream"
+        content="Are you sure you want to delete this stream ?"
+        actions={actions}
+      />
+    </div>
+  );
 };
 
 export default StreamDelete;
