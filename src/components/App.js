279dce69f73d1c162bedd497abded2e273b8cd2c
ramankarki <ramankarki40@gmail.com>
2021-01-06 21:33:27 +0545

    added : wildcard to streams route with id variable for individual streams

--- src/components/App.js
+++ src/components/App.js
@@ -15,7 +15,7 @@ export default function App() {
         <Header />
         <Route path="/" exact component={StreamList} />
         <Route path="/streams/new" exact component={StreamCreate} />
-        <Route path="/streams/show" exact component={StreamShow} />
+        <Route path="/streams/:id" exact component={StreamShow} />
         <Route path="/streams/edit/:id" exact component={StreamEdit} />
         <Route path="/streams/delete/:id" exact component={StreamDelete} />
       </section>
