9d92c25620683deb544d286d9c74098ea280a82b
ramankarki <ramankarki40@gmail.com>
2021-01-06 21:01:12 +0545

    used : wildcard and gave id variable on delete route

--- src/components/App.js
+++ src/components/App.js
@@ -17,7 +17,7 @@ export default function App() {
         <Route path="/streams/new" exact component={StreamCreate} />
         <Route path="/streams/show" exact component={StreamShow} />
         <Route path="/streams/edit/:id" exact component={StreamEdit} />
-        <Route path="/streams/delete" exact component={StreamDelete} />
+        <Route path="/streams/delete/:id" exact component={StreamDelete} />
       </section>
     </Router>
   );
